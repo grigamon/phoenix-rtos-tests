7373df69c121b981e98d21aeb5477fc680d16807
grigamon <gurigamonti@gmail.com>
2021-07-30 08:28:21 -0300

    Removed the sanity check of cosNormalCases test and added four controlled group of tests.

--- cos/testMathCos.c
+++ cos/testMathCos.c
@@ -1,9 +1,13 @@
-/* Test Cases groups (assuming those 5 high level requirements)
+/* Test Cases groups
 1. Accuracy of cos() functions for normal cases [cosNormalCases]
 2. Accuracy of cos() function in the edges [cosEdgesCases]
 3. Checking the parity of cos() function (cos x = cos(-x)) [cosParityCases]
 4. Checking the frequency of cos() function (cos x = cos (x+ T)) [cosPeriodicityCases]
 5. Checking the repeatability (since that the most of all cos() functions implementations use Maclaurin) [cosRepeatabilityCases]
+6. Accuracy of cos() function in the edges [cosEdgesCasesControl] using known values as inputs.
+7. Checking the parity of cos() function (cos x = cos(-x)) [cosParityCasesControl] using known values as inputs.
+8. Checking the frequency of cos() function (cos x = cos (x+ T)) [cosPeriodicityCasesControl] using known values as inputs.
+9. Checking the repeatability (since that the most of all cos() functions implementations use Maclaurin) [cosRepeatabilityCasesControl] using known values as inputs.
 */
 
 #include <math.h>
@@ -34,6 +38,7 @@ TEST_TEAR_DOWN(testCos)
 */
 }
 
+/*------------------------------------------------------------ Begin Exhaustives tests ------------------------------------------------------------*/
 
 /* 1. cosNormalCases
 Since we want to verify the accuracy of cos() function for normal cases, we will use remarkable angles as strategy to verify the results because their values are known.
@@ -64,9 +69,6 @@ TEST_ASSERT_FLOAT_WITHIN(TOLERANCE, 0.5, cos(5*M_PI/3));
 TEST_ASSERT_FLOAT_WITHIN(TOLERANCE, 0.70710, cos(7*M_PI/4));
 TEST_ASSERT_FLOAT_WITHIN(TOLERANCE, 0.86602, cos(11*M_PI/6));
 TEST_ASSERT_FLOAT_WITHIN(TOLERANCE, 1.0, cos(2*M_PI));
-
-//Sanity check test
-TEST_ASSERT_FLOAT_WITHIN(TOLERANCE, 0.5, cos(0));
 }
 
 
@@ -157,6 +159,160 @@ for (l=1; l<=MAXIMUM_ITERATION; l++)
 TEST_PASS();
 }
 
+/*------------------------------------------------------------ End Exhaustives tests ------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------ Begin control tests ------------------------------------------------------------*/
+
+/* 6.cosEdgesCasesControl
+The same idea of cosEdgesCases, but using known values.
+*/
+TEST(testCos, cosEdgesCasesControl)
+{
+//First quadrant
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,1.0, cos(0 + DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,0.86602, cos(M_PI/6 + DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,0.70710, cos(M_PI/4 + DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,0.5, cos(M_PI/3 + DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,0, cos(M_PI/2 + DELTA_RADIANS));
+
+//Second quadrant
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,-0.5, cos(2*M_PI/3 + DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,-0.70710, cos(3*M_PI/4 + DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,-0.86602, cos(5*M_PI/6 + DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,-1.0, cos(M_PI + DELTA_RADIANS));
+
+//Third quadrant
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,-0.86602, cos(7*M_PI/6 + DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,-0.70710, cos(5*M_PI/4 + DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,-0.5, cos(4*M_PI/3 + DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,0, cos(3*M_PI/2 + DELTA_RADIANS));
+
+//Fourth quadrant
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,0.5, cos(5*M_PI/3 + DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,0.70710, cos(7*M_PI/4 + DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,0.86602, cos(11*M_PI/6 + DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,1.0, cos(2*M_PI + DELTA_RADIANS));
+
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,1.0, cos(0 + DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,0.86602, cos(M_PI/6 + DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,0.70710, cos(M_PI/4 + DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,0.5, cos(M_PI/3 + DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,0, cos(M_PI/2 + DELTA_RADIANS));
+
+//Second quadrant
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,-0.5, cos(2*M_PI/3 - DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,-0.70710, cos(3*M_PI/4 - DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,-0.86602, cos(5*M_PI/6 - DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,-1.0, cos(M_PI - DELTA_RADIANS));
+
+//Third quadrant
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,-0.86602, cos(7*M_PI/6 - DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,-0.70710, cos(5*M_PI/4 - DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,-0.5, cos(4*M_PI/3 - DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,0, cos(3*M_PI/2 - DELTA_RADIANS));
+
+//Fourth quadrant
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,0.5, cos(5*M_PI/3 - DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,0.70710, cos(7*M_PI/4 - DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,0.86602, cos(11*M_PI/6 - DELTA_RADIANS));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,1.0, cos(2*M_PI - DELTA_RADIANS));
+}
+
+
+/*7.cosParityCasesControl
+The same idea of cosParityCasesControl, but using known values.
+*/
+TEST(testCos, cosParityCasesControl)
+{
+//First quadrant
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(-M_PI/6), cos(M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(-M_PI/4), cos(M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(-M_PI/3), cos(M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(-M_PI/2), cos(M_PI/2));
+
+//Second quadrant
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(-2*M_PI/3), cos(2*M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(-3*M_PI/4), cos(3*M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(-5*M_PI/6), cos(5*M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(-M_PI), cos(M_PI));
+
+//Third quadrant
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(-7*M_PI/6), cos(7*M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(-5*M_PI/4), cos(5*M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(-4*M_PI/3), cos(4*M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(-3*M_PI/2), cos(3*M_PI/2));
+
+//Fourth quadrant
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(-5*M_PI/3), cos(5*M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(-7*M_PI/4), cos(7*M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(-11*M_PI/6), cos(11*M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(-2*M_PI), cos(2*M_PI));
+}
+
+
+/*8.cosFrequencyCasesControl
+The same idea of cosFrequencyCases, but using known values.
+*/
+TEST(testCos, cosFrequencyCasesControl)
+{
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(M_PI/6 + 2*M_PI), cos(M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(M_PI/4 + 2*M_PI), cos(M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(M_PI/3 + 2*M_PI), cos(M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(M_PI/2 + 2*M_PI, cos(M_PI/2));
+
+//Second quadrant
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(2*M_PI/3 + 2*M_PI), cos(2*M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(3*M_PI/4 + 2*M_PI), cos(3*M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(5*M_PI/6 + 2*M_PI), cos(5*M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(M_PI + 2*M_PI), cos(M_PI));
+
+//Third quadrant
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(7*M_PI/6 + 2*M_PI), cos(7*M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(5*M_PI/4 + 2*M_PI), cos(5*M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(4*M_PI/3 + 2*M_PI), cos(4*M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(3*M_PI/2 + 2*M_PI), cos(3*M_PI/2));
+
+//Fourth quadrant
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(5*M_PI/3 + 2*M_PI), cos(5*M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(7*M_PI/4 + 2*M_PI), cos(7*M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(11*M_PI/6 + 2*M_PI), cos(11*M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(TOLERANCE,cos(2*M_PI + 2*M_PI), cos(2*M_PI));
+}
+
+
+/*9.cosRepeatabilityCasesControl
+The same idea of cosRepeatabilityCases, but using known values.
+*/
+TEST(testCos, cosRepeatabilityControl)
+{
+//First quadrant
+TEST_ASSERT_EQUAL_FLOAT(cos(M_PI/6), cos(M_PI/6));
+TEST_ASSERT_EQUAL_FLOAT(cos(M_PI/4), cos(M_PI/4));
+TEST_ASSERT_EQUAL_FLOAT(cos(M_PI/3), cos(M_PI/3));
+TEST_ASSERT_EQUAL_FLOAT(cos(M_PI/2), cos(M_PI/2));
+
+//Second quadrant
+TEST_ASSERT_EQUAL_FLOAT(cos(2*M_PI/3), cos(2*M_PI/3));
+TEST_ASSERT_EQUAL_FLOAT(cos(3*M_PI/4), cos(3*M_PI/4));
+TEST_ASSERT_EQUAL_FLOAT(cos(5*M_PI/6), cos(5*M_PI/6));
+TEST_ASSERT_EQUAL_FLOAT(cos(M_PI), cos(M_PI));
+
+//Third quadrant
+TEST_ASSERT_EQUAL_FLOAT(cos(7*M_PI/6), cos(7*M_PI/6));
+TEST_ASSERT_EQUAL_FLOAT(cos(5*M_PI/4), cos(5*M_PI/4));
+TEST_ASSERT_EQUAL_FLOAT(cos(4*M_PI/3), cos(4*M_PI/3));
+TEST_ASSERT_EQUAL_FLOAT(cos(3*M_PI/2), cos(3*M_PI/2));
+
+//Fourth quadrant
+TEST_ASSERT_EQUAL_FLOAT(cos(5*M_PI/3), cos(5*M_PI/3));
+TEST_ASSERT_EQUAL_FLOAT(cos(7*M_PI/4), cos(7*M_PI/4));
+TEST_ASSERT_EQUAL_FLOAT(cos(11*M_PI/6), cos(11*M_PI/6));
+TEST_ASSERT_EQUAL_FLOAT(cos(2*M_PI), cos(2*M_PI));
+}
+
+/*------------------------------------------------------------ End control tests ------------------------------------------------------------*/
+
 void runner(void)
 {
 	RUN_TEST_GROUP(testCos);
@@ -164,11 +320,18 @@ void runner(void)
 
 TEST_GROUP_RUNNER(testCos)
 {
+	//Running exhaustive tests
 	RUN_TEST_CASE(testCos, cosNormalCases);
 	RUN_TEST_CASE(testCos, cosEdgesCases);
 	RUN_TEST_CASE(testCos, cosParityCases);
 	RUN_TEST_CASE(testCos, cosPeriodicityCases);
 	RUN_TEST_CASE(testCos, cosRepeatabilityCases);
+	
+	//Running control tests
+	RUN_TEST_CASE(testCos, cosEdgesCasesControl);
+	RUN_TEST_CASE(testCos, cosParityCasesControl);
+	RUN_TEST_CASE(testCos, cosPeriodicityCasesControl);
+	RUN_TEST_CASE(testCos, cosRepeatabilityCasesControl);
 }
 
 int main(int argc, char *argv[])
