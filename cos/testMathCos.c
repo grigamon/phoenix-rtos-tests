471f5bd20cea847c7af474fe19f94ec237e5940f
grigamon <gurigamonti@gmail.com>
2021-07-29 21:02:16 -0300

    Missing semicolon

--- cos/testMathCos.c
+++ cos/testMathCos.c
@@ -34,29 +34,29 @@ TEST_TEAR_DOWN(testCos)
 TEST(testCos, cosNormalCases)
 {
 //First quadrant
-TEST_ASSERT_FLOAT_WITHIN(tolerance,1.0, cos(0))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,0.86602, cos(M_PI/6))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,0.70710, cos(M_PI/4))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,0.5, cos(M_PI/3))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,0, cos(M_PI/2))
+TEST_ASSERT_FLOAT_WITHIN(tolerance,1.0, cos(0));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,0.86602, cos(M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,0.70710, cos(M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,0.5, cos(M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,0, cos(M_PI/2));
 
 //Second quadrant
-TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.5, cos(2*M_PI/3))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.70710, cos(3*M_PI/4))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.86602, cos(5*M_PI/6))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,-1.0, cos(M_PI))
+TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.5, cos(2*M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.70710, cos(3*M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.86602, cos(5*M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,-1.0, cos(M_PI));
 
 //Third quadrant
-TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.86602, cos(7*M_PI/6))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.70710, cos(5*M_PI/4))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.5, cos(4*M_PI/3))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,0, cos(3*M_PI/2))
+TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.86602, cos(7*M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.70710, cos(5*M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.5, cos(4*M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,0, cos(3*M_PI/2));
 
 //Fourth quadrant
-TEST_ASSERT_FLOAT_WITHIN(tolerance,0.5, cos(5*M_PI/3))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,0.70710, cos(7*M_PI/4))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,0.86602, cos(11*M_PI/6))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,1.0, cos(2*M_PI))
+TEST_ASSERT_FLOAT_WITHIN(tolerance,0.5, cos(5*M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,0.70710, cos(7*M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,0.86602, cos(11*M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,1.0, cos(2*M_PI));
 }
 
 
@@ -64,29 +64,29 @@ TEST_ASSERT_FLOAT_WITHIN(tolerance,1.0, cos(2*M_PI))
 TEST(testCos, cosEdgesCases)
 {
 //First quadrant + deltaRadians
-TEST_ASSERT_FLOAT_WITHIN(tolerance,1.0, cos(0 + deltaRadians))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,0.86602, cos(M_PI/6 + deltaRadians))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,0.70710, cos(M_PI/4 + deltaRadians))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,0.5, cos(M_PI/3 + deltaRadians))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,0, cos(M_PI/2 + deltaRadians))
+TEST_ASSERT_FLOAT_WITHIN(tolerance,1.0, cos(0 + deltaRadians));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,0.86602, cos(M_PI/6 + deltaRadians));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,0.70710, cos(M_PI/4 + deltaRadians));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,0.5, cos(M_PI/3 + deltaRadians));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,0, cos(M_PI/2 + deltaRadians));
 
 //Second quadrant
-TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.5, cos(2*M_PI/3 + deltaRadians))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.70710, cos(3*M_PI/4 + deltaRadians))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.86602, cos(5*M_PI/6 + deltaRadians))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,-1.0, cos(M_PI + deltaRadians))
+TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.5, cos(2*M_PI/3 + deltaRadians));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.70710, cos(3*M_PI/4 + deltaRadians));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.86602, cos(5*M_PI/6 + deltaRadians));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,-1.0, cos(M_PI + deltaRadians));
 
 //Third quadrant
-TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.86602, cos(7*M_PI/6 + deltaRadians))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.70710, cos(5*M_PI/4 + deltaRadians))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.5, cos(4*M_PI/3 + deltaRadians))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,0, cos(3*M_PI/2 + deltaRadians))
+TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.86602, cos(7*M_PI/6 + deltaRadians));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.70710, cos(5*M_PI/4 + deltaRadians));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,-0.5, cos(4*M_PI/3 + deltaRadians));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,0, cos(3*M_PI/2 + deltaRadians));
 
 //Fourth quadrant
-TEST_ASSERT_FLOAT_WITHIN(tolerance,0.5, cos(5*M_PI/3 + deltaRadians))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,0.70710, cos(7*M_PI/4 + deltaRadians))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,0.86602, cos(11*M_PI/6 + deltaRadians))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,1.0, cos(2*M_PI + deltaRadians))
+TEST_ASSERT_FLOAT_WITHIN(tolerance,0.5, cos(5*M_PI/3 + deltaRadians));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,0.70710, cos(7*M_PI/4 + deltaRadians));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,0.86602, cos(11*M_PI/6 + deltaRadians));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,1.0, cos(2*M_PI + deltaRadians));
 }
 
 
@@ -94,56 +94,56 @@ TEST_ASSERT_FLOAT_WITHIN(tolerance,1.0, cos(2*M_PI + deltaRadians))
 TEST(testCos, cosParityCases)
 {
 //First quadrant
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-M_PI/6), cos(M_PI/6))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-M_PI/4), cos(M_PI/4))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-M_PI/3), cos(M_PI/3))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-M_PI/2, cos(M_PI/2))
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-M_PI/6), cos(M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-M_PI/4), cos(M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-M_PI/3), cos(M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-M_PI/2, cos(M_PI/2));
 
 //Second quadrant
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-2*M_PI/3), cos(2*M_PI/3))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-3*M_PI/4), cos(3*M_PI/4))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-5*M_PI/6), cos(5*M_PI/6))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-M_PI), cos(M_PI))
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-2*M_PI/3), cos(2*M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-3*M_PI/4), cos(3*M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-5*M_PI/6), cos(5*M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-M_PI), cos(M_PI));
 
 //Third quadrant
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-7*M_PI/6), cos(7*M_PI/6))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-5*M_PI/4), cos(5*M_PI/4))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-4*M_PI/3), cos(4*M_PI/3))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-3*M_PI/2), cos(3*M_PI/2))
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-7*M_PI/6), cos(7*M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-5*M_PI/4), cos(5*M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-4*M_PI/3), cos(4*M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-3*M_PI/2), cos(3*M_PI/2));
 
 //Fourth quadrant
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-5*M_PI/3), cos(5*M_PI/3))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-7*M_PI/4), cos(7*M_PI/4))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-11*M_PI/6), cos(11*M_PI/6))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-2*M_PI), cos(2*M_PI))
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-5*M_PI/3), cos(5*M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-7*M_PI/4), cos(7*M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-11*M_PI/6), cos(11*M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(-2*M_PI), cos(2*M_PI));
 }
 
 
 //4.cosFrequencyCases
 TEST(testCos, cosFrequencyCases)
 {
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(M_PI/6 + 2*M_PI), cos(M_PI/6))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(M_PI/4 + 2*M_PI), cos(M_PI/4))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(M_PI/3), cos(M_PI/3))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(M_PI/2, cos(M_PI/2))
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(M_PI/6 + 2*M_PI), cos(M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(M_PI/4 + 2*M_PI), cos(M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(M_PI/3), cos(M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(M_PI/2, cos(M_PI/2));
 
 //Second quadrant
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(2*M_PI/3 + 2*M_PI), cos(2*M_PI/3))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(3*M_PI/4 + 2*M_PI), cos(3*M_PI/4))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(5*M_PI/6 + 2*M_PI), cos(5*M_PI/6))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(M_PI + 2*M_PI), cos(M_PI))
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(2*M_PI/3 + 2*M_PI), cos(2*M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(3*M_PI/4 + 2*M_PI), cos(3*M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(5*M_PI/6 + 2*M_PI), cos(5*M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(M_PI + 2*M_PI), cos(M_PI));
 
 //Third quadrant
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(7*M_PI/6 + 2*M_PI), cos(7*M_PI/6))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(5*M_PI/4 + 2*M_PI), cos(5*M_PI/4))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(4*M_PI/3 + 2*M_PI), cos(4*M_PI/3))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(3*M_PI/2 + 2*M_PI), cos(3*M_PI/2))
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(7*M_PI/6 + 2*M_PI), cos(7*M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(5*M_PI/4 + 2*M_PI), cos(5*M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(4*M_PI/3 + 2*M_PI), cos(4*M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(3*M_PI/2 + 2*M_PI), cos(3*M_PI/2));
 
 //Fourth quadrant
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(5*M_PI/3 + 2*M_PI), cos(5*M_PI/3))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(7*M_PI/4 + 2*M_PI), cos(7*M_PI/4))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(11*M_PI/6 + 2*M_PI), cos(11*M_PI/6))
-TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(2*M_PI + 2*M_PI), cos(2*M_PI))
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(5*M_PI/3 + 2*M_PI), cos(5*M_PI/3));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(7*M_PI/4 + 2*M_PI), cos(7*M_PI/4));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(11*M_PI/6 + 2*M_PI), cos(11*M_PI/6));
+TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(2*M_PI + 2*M_PI), cos(2*M_PI));
 
 }
 
@@ -151,28 +151,28 @@ TEST_ASSERT_FLOAT_WITHIN(tolerance,cos(2*M_PI + 2*M_PI), cos(2*M_PI))
 TEST(testCos, cosRepeatability)
 {
 //First quadrant
-TEST_ASSERT_EQUAL_FLOAT(cos(M_PI/6), cos(M_PI/6))
-TEST_ASSERT_EQUAL_FLOAT(cos(M_PI/4), cos(M_PI/4))
-TEST_ASSERT_EQUAL_FLOAT(cos(M_PI/3), cos(M_PI/3))
-TEST_ASSERT_EQUAL_FLOAT(cos(M_PI/2, cos(M_PI/2))
+TEST_ASSERT_EQUAL_FLOAT(cos(M_PI/6), cos(M_PI/6));
+TEST_ASSERT_EQUAL_FLOAT(cos(M_PI/4), cos(M_PI/4));
+TEST_ASSERT_EQUAL_FLOAT(cos(M_PI/3), cos(M_PI/3));
+TEST_ASSERT_EQUAL_FLOAT(cos(M_PI/2, cos(M_PI/2));
 
 //Second quadrant
-TEST_ASSERT_EQUAL_FLOAT(cos(2*M_PI/3), cos(2*M_PI/3))
-TEST_ASSERT_EQUAL_FLOAT(cos(3*M_PI/4), cos(3*M_PI/4))
-TEST_ASSERT_EQUAL_FLOAT(cos(5*M_PI/6), cos(5*M_PI/6))
-TEST_ASSERT_EQUAL_FLOAT(cos(M_PI), cos(M_PI))
+TEST_ASSERT_EQUAL_FLOAT(cos(2*M_PI/3), cos(2*M_PI/3));
+TEST_ASSERT_EQUAL_FLOAT(cos(3*M_PI/4), cos(3*M_PI/4));
+TEST_ASSERT_EQUAL_FLOAT(cos(5*M_PI/6), cos(5*M_PI/6));
+TEST_ASSERT_EQUAL_FLOAT(cos(M_PI), cos(M_PI));
 
 //Third quadrant
-TEST_ASSERT_EQUAL_FLOAT(cos(7*M_PI/6), cos(7*M_PI/6))
-TEST_ASSERT_EQUAL_FLOAT(cos(5*M_PI/4), cos(5*M_PI/4))
-TEST_ASSERT_EQUAL_FLOAT(cos(4*M_PI/3), cos(4*M_PI/3))
-TEST_ASSERT_EQUAL_FLOAT(cos(3*M_PI/2), cos(3*M_PI/2))
+TEST_ASSERT_EQUAL_FLOAT(cos(7*M_PI/6), cos(7*M_PI/6));
+TEST_ASSERT_EQUAL_FLOAT(cos(5*M_PI/4), cos(5*M_PI/4));
+TEST_ASSERT_EQUAL_FLOAT(cos(4*M_PI/3), cos(4*M_PI/3));
+TEST_ASSERT_EQUAL_FLOAT(cos(3*M_PI/2), cos(3*M_PI/2));
 
 //Fourth quadrant
-TEST_ASSERT_EQUAL_FLOAT(cos(5*M_PI/3), cos(5*M_PI/3))
-TEST_ASSERT_EQUAL_FLOAT(cos(7*M_PI/4), cos(7*M_PI/4))
-TEST_ASSERT_EQUAL_FLOAT(cos(11*M_PI/6), cos(11*M_PI/6))
-TEST_ASSERT_EQUAL_FLOAT(cos(2*M_PI), cos(2*M_PI))
+TEST_ASSERT_EQUAL_FLOAT(cos(5*M_PI/3), cos(5*M_PI/3));
+TEST_ASSERT_EQUAL_FLOAT(cos(7*M_PI/4), cos(7*M_PI/4));
+TEST_ASSERT_EQUAL_FLOAT(cos(11*M_PI/6), cos(11*M_PI/6));
+TEST_ASSERT_EQUAL_FLOAT(cos(2*M_PI), cos(2*M_PI));
 }
 
 void runner(void)
